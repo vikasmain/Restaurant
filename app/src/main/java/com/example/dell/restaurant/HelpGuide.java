6e8a1ffc7bc99d868fab7666ba171809ad351d8b
dell <9716588439vik@gmail.com>
2016-08-14 07:06:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/restaurant/HelpGuide.java
@@ -0,0 +1,18 @@
+package com.example.dell.restaurant;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.dell.restaurant.R;
+
+public class HelpGuide extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        Button help;
+        super.onCreate(savedInstanceState);
+
+        // initialise form widget
+        setContentView(R.layout.help);
+
+    }
+}
